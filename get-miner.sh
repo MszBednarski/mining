841631222a313d31e9ee5e01b01a5c7e0bd3e42f
MszBednarski <msz.bednarski@gmail.com>
2022-03-12 10:16:36 +0000

    asfe

--- get-miner.sh
+++ get-miner.sh
@@ -1,9 +1,43 @@
-#!/bin/bash
-export ADDR=49ERscAZekwbCDZ3MqV2rhEwTqx1UbihGFCeSRK7Vsy8f6EA5xH151JRmvhAoV2sta2U9eV6CgeNt7u6jTfQ4WtYGbGQygd
+#!/bin/sh -e
 sudo apt-get update
-sudo apt-get install -y curl
-curl https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz -O
+sudo apt-get install -y curl msr-tools
+MSR_FILE=/sys/module/msr/parameters/allow_writes
+
+if test -e "$MSR_FILE"; then
+	echo on > $MSR_FILE
+else
+	modprobe msr allow_writes=on
+fi
+
+if grep -E 'AMD Ryzen|AMD EPYC' /proc/cpuinfo > /dev/null;
+	then
+	if grep "cpu family[[:space:]]:[[:space:]]25" /proc/cpuinfo > /dev/null;
+		then
+			echo "Detected Zen3 CPU"
+			wrmsr -a 0xc0011020 0x4480000000000
+			wrmsr -a 0xc0011021 0x1c000200000040
+			wrmsr -a 0xc0011022 0xc000000401500000
+			wrmsr -a 0xc001102b 0x2000cc14
+			echo "MSR register values for Zen3 applied"
+		else
+			echo "Detected Zen1/Zen2 CPU"
+			wrmsr -a 0xc0011020 0
+			wrmsr -a 0xc0011021 0x40
+			wrmsr -a 0xc0011022 0x1510000
+			wrmsr -a 0xc001102b 0x2000cc16
+			echo "MSR register values for Zen1/Zen2 applied"
+		fi
+elif grep "Intel" /proc/cpuinfo > /dev/null;
+	then
+		echo "Detected Intel CPU"
+		wrmsr -a 0x1a4 0xf
+		echo "MSR register values for Intel applied"
+else
+	echo "No supported CPU detected"
+fi
+export ADDR=49ERscAZekwbCDZ3MqV2rhEwTqx1UbihGFCeSRK7Vsy8f6EA5xH151JRmvhAoV2sta2U9eV6CgeNt7u6jTfQ4WtYGbGQygd
+curl -Lv https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz -O
 # 49ERscAZekwbCDZ3MqV2rhEwTqx1UbihGFCeSRK7Vsy8f6EA5xH151JRmvhAoV2sta2U9eV6CgeNt7u6jTfQ4WtYGbGQygd
 # unpack
-tar -xvzf xmrig-6.16.4-focal-x64.tar.gz
-./xmrig -o pool.minexmr.com:4444 -u $ADDR
+tar -xvzf xmrig-6.16.4-linux-x64.tar.gz
+cd xmrig-6.16.4 && sudo chmod 0700 xmrig && sudo ./xmrig -o pool.minexmr.com:4444 -u $ADDR
